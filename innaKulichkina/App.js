6af64376b99cd9b199686fceb61890a578d0202c
Inna <90662377+innk88@users.noreply.github.com>
2024-10-10 19:49:05 +0900

    Update App.js

--- innaKulichkina/App.js
+++ innaKulichkina/App.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Animated } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, Animated } from "react-native";
 
 export default function App() {
-  const [color, setColor] = useState('blue');
+  const [color, setColor] = useState("blue");
   const [position, setPosition] = useState(new Animated.Value(0));
 
-
   const handlePress = () => {
-    const newColor = color === 'blue' ? 'green' : 'blue';
+    const newColor = color === "blue" ? "green" : "blue";
     setColor(newColor);
     Animated.timing(position, {
-      toValue: position._value === 0 ? 100 : 0, 
+      toValue: position._value === 0 ? 100 : 0,
       duration: 500,
       useNativeDriver: true,
     }).start();
@@ -32,12 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   rectangle: {
     width: 100,
     height: 100,
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
   },
 });
