1a6e8e40bda8a71cece3519a3e0daaa1052af0cd
L1ghtowo2803 <90662386+L1ghtowo2803@users.noreply.github.com>
2024-12-19 14:59:09 +0900

    added Lab1 Lab2 Lab3
    Lab1: UseState
    Lab2: UseMemo
    Lab3: StateManager(redux)

--- nikolaevEvgeniy-IVT-21-2/App.js
+++ nikolaevEvgeniy-IVT-21-2/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Lab1 from "./Screens/Lab1";
+import Lab2 from "./Screens/Lab2";
+import Lab3 from "./Screens/Lab3";
+import TaskList from "./Screens/TaskList";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Lab1" component={Lab1} />
+          <Tab.Screen name="Lab2" component={Lab2} />
+          <Tab.Screen name="Lab3" component={Lab3} />
+          <Tab.Screen name="Tasks" component={TaskList} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
