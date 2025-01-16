f9029e5d41f8d663ee32cfc61026d6aecbce96bd
Emerald34 <nyrgun03@mail.ru>
2025-01-16 17:07:30 +0900

    Lab3(use-memo)

---
+++ nyrgunZakharov/screens/use-memo.js
@@ -0,0 +1,256 @@
+import React, { useEffect, useState, useMemo, useCallback, useRef } from "react";
+import { View, Text, ActivityIndicator, FlatList, StyleSheet, TouchableOpacity } from "react-native";
+
+const Lab3 = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [useMemoEnabled, setUseMemoEnabled] = useState(true);
+    const [timerEnabled, setTimerEnabled] = useState(true);
+    const [fetchTime, setFetchTime] = useState(null);
+    const [memoTime, setMemoTime] = useState(null);
+
+
+    const fetchData = useCallback(async () => {
+        setLoading(true);
+        const startTime = timerEnabled ? performance.now() : null; 
+
+        try {
+            const response = await fetch(
+                "https://api.coindesk.com/v1/bpi/currentprice.json"
+            );
+            if (!response.ok) {
+                throw new Error(`Network response was not ok, status: ${response.status}`);
+            }
+            const result = await response.json();
+            setData(result);
+            setError(null);
+            if(timerEnabled && startTime){
+                const endTime = performance.now();
+                setFetchTime(endTime - startTime); 
+             }
+        } catch (err) {
+            setError(err);
+            setData(null);
+            setFetchTime(null);
+        } finally {
+            setLoading(false);
+        }
+    }, [timerEnabled]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    const processedData = useMemo(() => {
+         const startTime = timerEnabled ? performance.now() : null; 
+        let result;
+        if (!data || !useMemoEnabled) {
+          let processed = data ? { updated: data?.time?.updated, rates: data?.bpi } : null
+            if(processed) {
+               for(let i = 0; i<10000; i++){
+                   let _ = Math.random()
+               }
+            }
+           result = processed
+        } else {
+            result = {
+                updated: data.time?.updated,
+                rates: data.bpi,
+            };
+        }
+          if(timerEnabled && startTime){
+                const endTime = performance.now();                
+                setMemoTime(endTime - startTime);  
+              }
+        return result;
+     }, [data, useMemoEnabled, timerEnabled]); 
+ 
+ 
+     const toggleUseMemo = () => {
+         setUseMemoEnabled(!useMemoEnabled);
+     };
+      const toggleTimer = () => {
+         setTimerEnabled(!timerEnabled);
+     };
+ 
+ 
+     if (loading)
+         return (
+             <View style={styles.loaderContainer}>
+                 <ActivityIndicator size="large" color="#00ff00" />
+             </View>
+         );
+     if (error)
+         return (
+             <View style={styles.errorContainer}>
+                 <Text style={styles.errorText}>Error: {error?.message}</Text>
+             </View>
+         );
+ 
+     if (!processedData) {
+         return (<View style={styles.container}>
+             <Text style={[styles.title, { color: "#000" }]}>
+                 Lab 2
+             </Text>
+             <Text
+                 style={[
+                     styles.subtitle,
+                     { color: "#666" },
+                 ]}
+             >Loading data...
+             </Text>
+         </View>);
+ 
+     }
+ 
+ 
+     return (
+         <View
+             style={[
+                 styles.container,
+                 { backgroundColor: "#fff" },
+             ]}
+         >
+             <Text style={[styles.title, { color: "#000" }]}>
+                 Lab 3
+             </Text>
+             <Text
+                 style={[
+                     styles.subtitle,
+                     { color: "#666" },
+                 ]}
+             >
+                 Current Bitcoin Prices
+             </Text>
+             <Text
+                 style={[
+                     styles.updatedText,
+                     { color: "#000" },
+                 ]}
+             >
+                 Last updated: {processedData?.updated}
+             </Text>
+              {timerEnabled && (
+                 <Text style={[styles.timerText, { color: "#000" }]}>
+                      Fetch Time: {fetchTime !== null ? `${fetchTime.toFixed(2)} ms` : 'Loading...'}
+                 </Text>
+              )}
+              {timerEnabled && (
+                  <Text style={[styles.timerText, { color: "#000" }]}>
+                  Memo Time (UseMemo {useMemoEnabled ? "On" : "Off"}): {memoTime !== null ? `${memoTime.toFixed(2)} ms` : 'Loading...'}
+              </Text>
+          )}
+          <View style={styles.buttonsContainer}>
+              <TouchableOpacity style={styles.button} onPress={fetchData}>
+                  <Text style={styles.buttonText}>Обновить</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={toggleUseMemo}>
+                  <Text style={styles.buttonText}>
+                      {useMemoEnabled ? "Выключить useMemo" : "Включить useMemo"}
+                  </Text>
+              </TouchableOpacity>
+          </View>
+          <FlatList
+              data={Object.entries(processedData.rates)}
+              keyExtractor={([currency]) => currency}
+              renderItem={({ item }) => {
+                  const [currency, info] = item;
+                  return (
+                      <View
+                          style={[
+                              styles.rateContainer,{
+                                backgroundColor: "#e0e0e0",
+                            },
+                        ]}
+                    >
+                        <Text
+                            style={[
+                                styles.rateText,
+                                { color: "#000" },
+                            ]}
+                        >
+                            {info.code}: {info.rate}
+                        </Text>
+                    </View>
+                );
+            }}
+        />
+    </View>
+);
+};
+
+const styles = StyleSheet.create({
+loaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+},
+errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+},
+errorText: {
+    fontSize: 18,
+    color: "red",
+},
+container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+},
+title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10,
+},
+subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+},
+updatedText: {
+    fontSize: 16,
+    marginBottom: 20,
+},
+rateContainer: {
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 5,
+    alignSelf: "stretch",
+    borderColor: 'black',
+    borderWidth: 2,
+},
+rateText: {
+    fontSize: 18,
+    textAlign: "center",
+},
+buttonsContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    width: '60%',
+    marginBottom: 20,
+},
+button: {
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 5,
+    alignSelf: "stretch",
+    borderColor: 'black',
+    borderWidth: 2,
+    backgroundColor: 'green',
+    borderRadius: 5,
+},
+buttonText: {
+    color: 'black',
+    fontSize: 16,
+    textAlign: 'center',
+},
+timerText:{
+    fontSize: 12,
+}
+});
+
+export default Lab3;
