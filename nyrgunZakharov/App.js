987770bf4ec50fd420de6e56a669b0eabbbadb8f
Emerald34 <nyrgun03@mail.ru>
2025-01-17 22:17:26 +0900

    Update App.js

--- nyrgunZakharov/App.js
+++ nyrgunZakharov/App.js
@@ -40,16 +40,12 @@ const AppContent = () => {
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AppContent />
-    </ThemeProvider>
-
-      <NavigationContainer>
+    <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Lab1" component={Lab1} />
 
           <Tab.Screen name="Lab2" component={Lab2} />
-
+          <Tab.Screen name="Lab3" component={Lab3} />
         </Tab.Navigator>
       </NavigationContainer>
   );
